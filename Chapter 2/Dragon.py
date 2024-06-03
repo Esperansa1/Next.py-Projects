8bcc7d914366e1fea19548fb00658ee96093865c
Or Esperansa <6or2005@gmail.com>
2024-06-03 20:02:09 +0300

    Added stink_count for skunk and color for dragon

--- Chapter 2/Dragon.py
+++ Chapter 2/Dragon.py
@@ -1,10 +1,14 @@
 from Animal import Animal
 class Dragon(Animal):
-    def __init__(self, name, hunger):
+    def __init__(self, name, hunger, color="Green"):
         super().__init__(name, hunger)
+        self._color = color
 
     def get_type(self):
         return "Dragon"
+
+    def get_color(self):
+        return color
 
     def talk(self):
         return "Raaaawr"
--- Chapter 2/Skunk.py
+++ Chapter 2/Skunk.py
@@ -1,12 +1,16 @@
 from Animal import Animal
 class Skunk(Animal):
-    def __init__(self, name, hunger):
+    def __init__(self, name, hunger, stink_count = 6):
         super().__init__(name, hunger)
         self._stink_count = 6
         self._color = "Green"
+        self._stink_count = self._stink_count
 
     def get_type(self):
         return "Skunk"
+
+    def get_stink_count(self):
+        return self._stink_count
 
     def talk(self):
         return "tsssss"
